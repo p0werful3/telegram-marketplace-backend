723d141a36af12f2569652f05dfa36ab679c3acd
p0werful3 <129072492+p0werful3@users.noreply.github.com>
2026-03-13 00:06:28 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 web_app = WebAppInfo(
-    url="https://p0werful3.github.io/telegram-marketplace-miniapp/?v=131"
+    url="https://p0werful3.github.io/telegram-marketplace-miniapp/?v=132"
 )
 
 keyboard = ReplyKeyboardMarkup(
@@ -34,5 +34,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-
