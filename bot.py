a03ec0a678cd4bb2ae0bb529daa5ca867086a6c2
p0werful3 <129072492+p0werful3@users.noreply.github.com>
2026-03-16 01:04:15 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 web_app = WebAppInfo(
-    url="https://p0werful3.github.io/telegram-marketplace-miniapp/?v=269"
+    url="https://p0werful3.github.io/telegram-marketplace-miniapp/?v=270"
 )
 
 keyboard = ReplyKeyboardMarkup(
